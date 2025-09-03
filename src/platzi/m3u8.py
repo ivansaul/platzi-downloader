742601570b8330fba4fc61f7fb05171fab33d4ad
alpha-drm <alpha_drm@outlook.com>
2025-09-03 20:27:19 -0300

    Add files via upload
    new features: progress bar format, quality selection

--- src/platzi/m3u8.py
+++ src/platzi/m3u8.py
@@ -13,7 +13,6 @@ from tqdm.asyncio import tqdm
 
 from .helpers import retry
 
-
 def ffmpeg_required(func):
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
@@ -34,7 +33,7 @@ def _extract_streaming_urls(content: str) -> list[str] | None:
     pattern = r"(https?://[^\s]+|(?::)?///?[^\s]+)"
     matches = re.findall(pattern, content)
 
-    urls = []
+    urls = [] # Se guarda las resoluciones del video
     for match in matches:
         if match.startswith("http"):
             urls.append(match)
@@ -54,7 +53,7 @@ async def _ts_dl(url: str, path: Path, **kwargs):
     path.unlink(missing_ok=True)
     path.parent.mkdir(parents=True, exist_ok=True)
 
-    client = rnet.Client(impersonate=rnet.Impersonate.Firefox135)
+    client = rnet.Client(impersonate=rnet.Impersonate.Firefox139)
     response: rnet.Response = await client.get(url, **kwargs)
 
     try:
@@ -77,7 +76,10 @@ async def _worker_ts_dl(urls: list, dir: Path, **kwargs):
     BATCH_SIZE = 5
     IDX = 1
 
-    with tqdm(total=len(urls)) as bar:
+    # --- NEW FEATURES: progress bar format ---
+    bar_format="{desc} |{bar}|{percentage:3.0f}% [{n_fmt}/{total_fmt} fragments] [{elapsed}<{remaining}, {rate_fmt}{postfix}]"
+    
+    with tqdm(total=len(urls), desc="Progress", colour='green', bar_format=bar_format, ascii='░█') as bar:
         for i in range(0, len(urls), BATCH_SIZE):
             urls_batch = urls[i : i + BATCH_SIZE]
             tasks = []
@@ -114,7 +116,7 @@ async def _m3u8_dl(
     path.parent.mkdir(parents=True, exist_ok=True)
     tmp_dir.mkdir(parents=True, exist_ok=True)
 
-    client = rnet.Client(impersonate=rnet.Impersonate.Firefox135)
+    client = rnet.Client(impersonate=rnet.Impersonate.Firefox139)
     response: rnet.Response = await client.get(url, **kwargs)
 
     try:
@@ -190,27 +192,32 @@ async def m3u8_dl(
     :param kwargs: Additional keyword arguments to pass to the requests client.
     :return: None
     """
-    # TODO: implement quality selection
-
+    
+    # --- NEW FEATURES: implement quality selection ---
+    
+    quality = kwargs.get("quality", False)
+    
+    quality = 0 if quality == '720' else 1
+   
     overwrite = kwargs.get("overwrite", False)
     path = path if isinstance(path, Path) else Path(path)
 
     if not overwrite and path.exists():
         return
 
-    client = rnet.Client(impersonate=rnet.Impersonate.Firefox135)
+    client = rnet.Client(impersonate=rnet.Impersonate.Firefox139)
     response: rnet.Response = await client.get(url, **kwargs)
 
     try:
         if not response.ok:
             raise Exception("Error downloading m3u8")
 
-        m3u8_urls = _extract_streaming_urls(await response.text())
-
+        m3u8_urls = _extract_streaming_urls(await response.text()) # El enlace .m3u8 contiene las resoluciones del video.
+ 
         if not m3u8_urls:
             raise Exception("No m3u8 urls found")
 
-        await _m3u8_dl(m3u8_urls[0], path, **kwargs)
+        await _m3u8_dl(m3u8_urls[int(quality)], path, **kwargs) # Aquí va la resolucion del video [0]=1280; [1]=1920
 
     except Exception:
         raise
