a6eea0d7ae8b4c5db2404f0b500f0e404019b2e1
alpha-drm <alpha_drm@outlook.com>
2025-09-03 20:29:36 -0300

    Add files via upload
    TODO: saving logs

--- src/platzi/logger.py
+++ src/platzi/logger.py
@@ -1,7 +1,13 @@
-import sys
-
+import sys, os
+from datetime import datetime
 from rich import print
 
+
+# log_filename = f"{datetime.now().strftime('%d-%m-%Y %H-%M-%S')}.log"
+# LOG_DIR = "logs"
+# if not os.path.exists(LOG_DIR):
+#     os.makedirs(LOG_DIR)   
+# log_filepath = os.path.join(LOG_DIR, log_filename) 
 
 class Logger:
     show_warnings = True
@@ -37,3 +43,24 @@ class Logger:
         Logger.clear()
         print(text, flush=True)
         cls.is_writing = False
+
+
+    # --- Testing saving logs ---
+    # @classmethod
+    # def _log(cls, text, head, color="green", end="\n"):
+    #     cls.is_writing = True
+    #     Logger.clear()
+    #     timestamp = datetime.now().strftime("[%d-%m-%Y %H:%M:%S]")
+    #     formatted_text = f"{timestamp} {head} {text}"
+    #     print(f"{timestamp} [{color}]{head} {text}[/{color}]", end=end, flush=True)
+    #     cls._write_to_file(formatted_text)
+    #     cls.is_writing = False
+
+    # @classmethod
+    # def _write_to_file(cls, text):
+    #     try:
+    #         with open(log_filepath, "a", encoding="utf-8") as f:
+    #             f.write(text + "\n")
+    #     except Exception as e:
+    #         # Fallback si hay error al escribir el log
+    #         sys.stderr.write(f"Error al escribir en el archivo de log: {e}\n")
